edcc196820c098376a07d57350fd0335db15ac7a
tnantoka <tnantoka@bornneet.com>
2023-02-20 22:35:38 +0900

    feat: evaluator

--- repl/repl.go
+++ repl/repl.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"io"
 
+	"def/evaluator"
 	"def/lexer"
+	"def/object"
 	"def/parser"
 )
 
@@ -13,6 +15,8 @@ const PROMPT = ">> "
 
 func Start(in io.Reader, out io.Writer) {
 	scanner := bufio.NewScanner(in)
+	env := object.NewEnvironment()
+
 	for {
 		fmt.Printf(PROMPT)
 		scanned := scanner.Scan()
@@ -30,8 +34,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program, env)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
